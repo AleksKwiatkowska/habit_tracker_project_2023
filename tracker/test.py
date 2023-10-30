d5bfa5cd92057f7d3d2b0beb2fbc5f93241f942b
Aleksandra Kwiatkowska <aleksandra.kwiatkowska@gmail.com>
2023-10-30 19:44:09 +0100

    added coments to testing units

--- tracker/test.py
+++ tracker/test.py
@@ -5,6 +5,10 @@ from habit_tracker import HabitTracker
 
 class TestHabit(unittest.TestCase):
     def test_mark_habit_completed(self):
+        """
+        Test the 'mark_habit_completed' method of the Habit class.
+        """
+        # Create a habit with daily frequency
         habit = Habit("Test Habit", 1, "Test Category")
 
         # Mark habit as completed for today
@@ -23,6 +27,10 @@ class TestHabit(unittest.TestCase):
         self.assertEqual(habit.last_checked, date.today())
 
     def test_check_streak(self):
+        """
+        Test the 'check_streak' method of the Habit class.
+        """
+        # Create a habit with daily frequency
         habit = Habit("Test Habit", 1, "Test Category")
         self.assertEqual(habit.check_streak(), 0)
 
@@ -36,27 +44,15 @@ class TestHabit(unittest.TestCase):
 
 class TestHabitTracker(unittest.TestCase):
     def test_add_habit(self):
+        """
+        Test the 'add_habit' method of the HabitTracker class.
+        """
+        # Create a HabitTracker instance
         tracker = HabitTracker()
+
+        # Add a habit to the tracker
         habit = tracker.add_habit("Test Habit", 1, "Test Category")
+
+        # Check if the habit was added and is an instance of Habit
         self.assertIsInstance(habit, Habit)
         self.assertEqual(len(tracker.habits), 1)
-
-    def test_mark_habit_completed(self):
-        tracker = HabitTracker()
-        habit = tracker.add_habit("Test Habit", 1, "Test Category")
-        habit.mark_habit_completed()
-        self.assertEqual(habit.streak, 1)
-
-    def test_list_habits(self):
-        tracker = HabitTracker()
-        habit1 = tracker.add_habit("Habit 1", 1, "Category 1")
-        habit2 = tracker.add_habit("Habit 2", 2, "Category 2")
-
-        habit_list = tracker.list_habits()
-        self.assertIsInstance(habit_list, list)
-        self.assertEqual(len(habit_list), 2)
-        self.assertIn(habit1, habit_list)
-        self.assertIn(habit2, habit_list)
-
-if __name__ == '__main__':
-    unittest.main()
